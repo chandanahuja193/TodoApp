2965dad7900a0b071f805756750bad08fc61cc68
chandan193 <chandanahuja93@gmail.com>
2019-12-11 20:06:33 +0530

    Updates

--- precache-manifest.b6be90c44b2d8c26e08b1b3f0900f2e2.js
+++ precache-manifest.b6be90c44b2d8c26e08b1b3f0900f2e2.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a53c967ba24e4df5bccaf0a3b971b5bd",
+    "revision": "cd5b191d2299fed0a6a065c4d0ba0148",
     "url": "/todoapp/index.html"
   },
   {
-    "revision": "6b7e34ab616d68b75f3a",
+    "revision": "2fed969c29bc8b6aa3dc",
     "url": "/todoapp/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "ae44db6802b117aea104",
+    "revision": "86caeaf6c35adfc5c034",
     "url": "/todoapp/static/css/main.2aa505f1.chunk.css"
   },
   {
-    "revision": "6b7e34ab616d68b75f3a",
-    "url": "/todoapp/static/js/2.a652b7ca.chunk.js"
+    "revision": "2fed969c29bc8b6aa3dc",
+    "url": "/todoapp/static/js/2.9b197f38.chunk.js"
   },
   {
     "revision": "86afaa925e0f432774bf95b19b6933fb",
-    "url": "/todoapp/static/js/2.a652b7ca.chunk.js.LICENSE"
+    "url": "/todoapp/static/js/2.9b197f38.chunk.js.LICENSE"
   },
   {
-    "revision": "ae44db6802b117aea104",
-    "url": "/todoapp/static/js/main.df2ffd21.chunk.js"
+    "revision": "86caeaf6c35adfc5c034",
+    "url": "/todoapp/static/js/main.90c285a2.chunk.js"
   },
   {
     "revision": "6a063160d20e2abd0b03",
