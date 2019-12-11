c06a75f92bc31e7c42eb4aeff8f33e048cca57b2
chandan193 <chandanahuja93@gmail.com>
2019-12-11 19:36:09 +0530

    Updates

---
+++ precache-manifest.b6be90c44b2d8c26e08b1b3f0900f2e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5b191d2299fed0a6a065c4d0ba0148",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "2fed969c29bc8b6aa3dc",
+    "url": "/todoapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "86caeaf6c35adfc5c034",
+    "url": "/todoapp/static/css/main.2aa505f1.chunk.css"
+  },
+  {
+    "revision": "2fed969c29bc8b6aa3dc",
+    "url": "/todoapp/static/js/2.9b197f38.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/todoapp/static/js/2.9b197f38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86caeaf6c35adfc5c034",
+    "url": "/todoapp/static/js/main.90c285a2.chunk.js"
+  },
+  {
+    "revision": "6a063160d20e2abd0b03",
+    "url": "/todoapp/static/js/runtime-main.9bf3d3f4.js"
+  }
+]);
